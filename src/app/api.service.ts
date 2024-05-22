28ab660e12e558507e5db70307bf6a6d687c2543
nanookOlive <olivier.stierer@gmail.com>
2024-05-22 11:56:37 +0200

    OLDRUNS-reach endpoint and show data ok

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -2,19 +2,31 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+export interface Outing{
+  id:number;
+  date:string;
+  nbKilometres:number;
+  duree:number;
+  avis:string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
   //url de l'api
-  private url : string = "http://localhost:8080/sortie/find/4"; 
+  private url : string = "http://localhost:8080/sortie/"; 
 
   //injection
   constructor(private client : HttpClient) {}
 
   //méthode pour fetch une sortie
   getOuting():Observable<any>{
-    return this.client.get(this.url);
+    return this.client.get(this.url+"find/4");
+  }
+
+  getAllOutings():Observable<{[key:string]:Outing}>{
+    return this.client.get<{[key:string]:Outing}>(this.url+"all");
   }
 }
--- src/app/old-runs/old-runs.component.ts
+++ src/app/old-runs/old-runs.component.ts
@@ -1,6 +1,13 @@
 import { Component,OnInit } from '@angular/core';
 import {ApiService} from '../api.service';
 
+export interface Outing{
+  id:number;
+  date:string;
+  nbKilometres:number;
+  duree:number;
+  avis:string;
+}
 @Component({
   selector: 'app-old-runs',
   templateUrl: './old-runs.component.html',
@@ -9,20 +16,26 @@ import {ApiService} from '../api.service';
 export class OldRunsComponent implements OnInit {
 
   //le resultat de la recherche
-  data :any;
+  outings  : {[key:string]:Outing}={}
+  
 
  constructor(private apiService : ApiService){}
 
  ngOnInit(){
 
-  this.apiService.getOuting().subscribe(
+
+  this.apiService.getAllOutings().subscribe(
     (response)=>{
-      this.data = response;
+      this.outings = response;
     },
     (error)=>{
       console.error('Error');
     }
   );
  
+ }
+
+ getKeys(obj:{[key:string]:any}):string[]{
+  return Object.keys(obj);
  }
 }
